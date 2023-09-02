12da9d01c68739c321c4e8ffd3ab970c2f6dff1d
sfolorunsho <sfolorunsho@thoughtmachine.net>
2023-09-02 11:00:46 +0100

    feat: support tagged strings

--- src/tokeniser/__fixtures__/taggedStrings.ts
+++ src/tokeniser/__fixtures__/taggedStrings.ts
@@ -139,4 +139,88 @@ export const TAGGED_STRING_FIXTURES: TestCase[] = [
       },
     ],
   },
+  {
+    input: 'f"I am {age} years old"',
+    expected: [
+      {
+        type: 'STRING',
+        value: 'f"I am {age} years old"',
+        startPos: {line: 1, column: 0},
+        endPos: {line: 1, column: 23},
+        lineNo: 1,
+        colOffset: 0,
+      },
+      {
+        type: 'EOF',
+        value: TOKENS.EOF,
+        startPos: {line: 1, column: 23},
+        endPos: {line: 1, column: 23},
+        lineNo: 1,
+        colOffset: 23,
+      },
+    ],
+  },
+  {
+    input: 'f"I am {height} and {\'a half\'} feet tall"',
+    expected: [
+      {
+        type: 'STRING',
+        value: 'f"I am {height} and {\'a half\'} feet tall"',
+        startPos: {line: 1, column: 0},
+        endPos: {line: 1, column: 41},
+        lineNo: 1,
+        colOffset: 0,
+      },
+      {
+        type: 'EOF',
+        value: TOKENS.EOF,
+        startPos: {line: 1, column: 41},
+        endPos: {line: 1, column: 41},
+        lineNo: 1,
+        colOffset: 41,
+      },
+    ],
+  },
+  {
+    input: 'f"I weigh {weight.to_stone()} stone"',
+    expected: [
+      {
+        type: 'STRING',
+        value: 'f"I weigh {weight.to_stone()} stone"',
+        startPos: {line: 1, column: 0},
+        endPos: {line: 1, column: 36},
+        lineNo: 1,
+        colOffset: 0,
+      },
+      {
+        type: 'EOF',
+        value: TOKENS.EOF,
+        startPos: {line: 1, column: 36},
+        endPos: {line: 1, column: 36},
+        lineNo: 1,
+        colOffset: 36,
+      },
+    ],
+  },
+  {
+    input: 'f"I weigh {weight.to_kg() * 7} kilograms"',
+    expected: [
+      {
+        type: 'STRING',
+        value: 'f"I weigh {weight.to_kg() * 7} kilograms"',
+        startPos: {line: 1, column: 0},
+        endPos: {line: 1, column: 41},
+        lineNo: 1,
+        colOffset: 0,
+      },
+      {
+        type: 'EOF',
+        value: TOKENS.EOF,
+        startPos: {line: 1, column: 41},
+        endPos: {line: 1, column: 41},
+        lineNo: 1,
+        colOffset: 41,
+      },
+    ],
+  },
 ];
