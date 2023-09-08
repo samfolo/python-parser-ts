37cd2721337d6ae5fba8de42c4a49fb652a7d5eb
sfolorunsho <sfolorunsho@thoughtmachine.net>
2023-09-08 20:56:27 +0100

    refactor: use actual python3 tokeniser to generate fixtures

--- src/tokeniser/__generator__/output.ts
+++ src/tokeniser/__generator__/output.ts
@@ -15,18 +15,48 @@ const FIXTURE = [
     "colOffset": 0
   },
   {
-    "type": "ENDMARKER",
-    "kind": "ENDMARKER",
-    "value": "",
+    "type": "STRING",
+    "kind": "STRING",
+    "value": "\"Hello world\"",
     "startPos": {
       "line": 1,
       "column": 0
     },
     "endPos": {
       "line": 1,
-      "column": 0
+      "column": 13
     },
     "lineNo": 1,
+    "colOffset": 0
+  },
+  {
+    "type": "NEWLINE",
+    "kind": "NEWLINE",
+    "value": "",
+    "startPos": {
+      "line": 1,
+      "column": 13
+    },
+    "endPos": {
+      "line": 1,
+      "column": 14
+    },
+    "lineNo": 1,
+    "colOffset": 13
+  },
+  {
+    "type": "ENDMARKER",
+    "kind": "ENDMARKER",
+    "value": "",
+    "startPos": {
+      "line": 2,
+      "column": 0
+    },
+    "endPos": {
+      "line": 2,
+      "column": 0
+    },
+    "lineNo": 2,
     "colOffset": 0
   }
 ]
